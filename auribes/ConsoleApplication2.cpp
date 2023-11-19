d105f52510ae400aa0d3aa2d22af640f1821624a
Lois Duplain <loisdupl42@gmail.com>
2023-11-19 18:33:37 +0100

    correction auribes

---
+++ auribes/ConsoleApplication2.cpp
@@ -0,0 +1,23 @@
+#include "Person.hpp"
+
+
+int main()
+{
+	Person::Person()
+	{
+		_fullName = "Driver";
+		_weight = 80;
+		_isTiedUp = 0;
+		say = "message";
+	}
+
+	Vehicle::Vehicle()
+	{
+		_brand = BMW;
+		_weight = 1000;
+		_maxPassengers = 0;
+
+
+	}
+
+}
---
+++ auribes/Person.hpp
@@ -0,0 +1,31 @@
+#include <string>
+#include <list>
+
+
+	class Person 
+	{
+	private:
+
+		std::string _fullName;
+		int _weight;
+		bool _isTiedUp;
+		std::string say;
+
+	public:
+};
+
+	
+	
+	
+	class Vehicle
+	{
+	private:
+		std::string _brand;
+		int _weight;
+		std::list _passengers;
+		int _maxPassengers;
+
+	public:
+	};
+
+	
